647fabab00fb78b9077a273067f3cadd8e588c20
Gulaba  Ram <gulabgkg99@gmail.com>
2025-12-27 21:28:41 +0530

    fix error

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,29 @@
 
 const express=require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 require('dotenv').config();
 
+const PORT =process.env.PORT || 3000;
+
 const dbconnect = require("./config/db");
 dbconnect.db()
+
+// middlewares
+app.use(express.json());       // parse JSON body
+app.use(cookieParser());       // parse cookies
+
+///auth routes  
+const authRoute = require("./routes/authRoutes");
+
+//routes mountesss
+app.use("/api/v1/auth",authRoute);
 
 app.get('/', (req, res) => {
   res.send("API running");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
