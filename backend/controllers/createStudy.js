326a69d996c4f24bdee6a2bc37b5ef3222855177
Gulaba  Ram <gulabgkg99@gmail.com>
2025-12-28 19:41:35 +0530

    create study controllers

---
+++ backend/controllers/createStudy.js
@@ -0,0 +1,15 @@
+const User= require("../models/User");
+const Study = require("../models/Study");
+
+const createStudy = async (req, res)=>{
+    try{
+       const {title ,description , subject ,status } = req.body;
+
+       
+
+    }
+    catch(err){
+
+    }
+}
+  
