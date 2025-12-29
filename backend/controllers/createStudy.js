130c6de6a451097029e1d6b09ed51658fdfcdee8
Gulaba  Ram <gulabgkg99@gmail.com>
2025-12-29 20:38:47 +0530

    backend almost done

--- backend/controllers/createStudy.js
+++ backend/controllers/createStudy.js
@@ -72,5 +72,98 @@ const getstudies = async(req,res)=>{
 
     }
 }
+
+const deletestudies=async(req,res)=>{
+
+    try{
+        const userId = req.user._id;
+    const studyId = req.params.id;
+
+    if(!studyId){
+        return res.status(400).json({
+            success:false,
+            message:"Study Id is required",
+        })
+    }
+    
+    if(!userId){
+        return res.status(400).json({
+            success:false,
+            message:"user not found",
+        })
+    }
+
+    const study = await Study.findOne({
+        _id : studyId,
+        user:userId,
+    })
+
+     
+    if(!study){
+        return res.status(400).json({
+            success:false,
+            message:"study not found or not authrized",
+        })
+    }
+
+    await Study.findByIdAndDelete(studyId);
+
+
+    return res.status(200).json({
+        success:true,
+        message:"study delete successfully ",
+    })
+
+
+
+    }
+    catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"Intternal server error",
+        })
+    }
+
+}
+
+const updatestudy = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const studyId = req.params.id;
+    const { title, description, status } = req.body;
+
+    const study = await Study.findOne({
+      _id: studyId,
+      user: userId,
+    });
+
+    if (!study) {
+      return res.status(404).json({
+        success: false,
+        message: "Study not found or not authorized",
+      });
+    }
+
+    // update only provided fields
+    if (title) study.title = title;
+    if (description) study.description = description;
+    if (status) study.status = status;
+
+    await study.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Study updated successfully",
+      study,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: err.message,
+    });
+  }
+};
+
  
-module.exports = { createStudy,getstudies };
+module.exports = { createStudy,getstudies,deletestudies,updatestudy };
--- backend/routes/StudyRoutes.js
+++ backend/routes/StudyRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {createStudy,getstudies} = require("../controllers/createStudy");
+const {createStudy,getstudies,deletestudies,updatestudy} = require("../controllers/createStudy");
 const {authmiddleware} = require("../middleware/authmiddleware");
 
 router.post("/createstudy", authmiddleware,createStudy);
 router.get("/getstudies",authmiddleware,getstudies);
+router.delete("/delete/:id", authmiddleware, deletestudies);
+router.put("/update/:id",authmiddleware,updatestudy);
+
 module.exports = router;
 
